b8cadb5fd5124a42b44e85771d53f8b3628318e0
Jmaonri Covarrubias <jmaonri@gmail.com>
2018-05-29 08:59:36 -0600

    1.4.2
    Code clean up.
    I don't have Token.py at the moment so I can't run felix to see if he works

--- Felix.py
+++ Felix.py
@@ -5,42 +5,48 @@ import Token #imports Token.py which holds the bot token
 import asyncio
 import time
 
+#Declare variables
+sendMessage = BotClient.send_message()
+messageStarts = message.content.lower().startswith()
+githubLink = "https://github.com/Jmaonri/Felix"
+twitchLink = 'https://www.twitch.tv/thelittledude_ld'
+Streaming = 1
 Client = discord.Client()
-client = commands.Bot(command_prefix = "!")
+botClient = commands.Bot(command_prefix = "!")
+sendCommands = "```\nHere are all the commands:\n\n!github - Link to the github of Felix Bot\n!alive? - Checking for my pulse I see...\n!gay - Use at your own risk... \n!nut - Pretty self explanatory```"
 
-felixPics = [0, 1]
-felixPics[0] = "https://myanimelist.cdn-dena.com/images/characters/5/353573.jpg"
-felixPics[1] = "https://www.dailydot.com/wp-content/uploads/228/6f/ab64a69d4cd848bad23ed0f9190a91eb.jpg"
+imageList = [0, 1]
+imageList[0] = "https://myanimelist.cdn-dena.com/images/characters/5/353573.jpg" #despacito
+imageList[1] = "https://www.dailydot.com/wp-content/uploads/228/6f/ab64a69d4cd848bad23ed0f9190a91eb.jpg"
 
 #Startup Log
 @client.event
 async def on_ready():
-    print(f"{client.user.name} is online and running")
+    print(f"{botClient.user.name} is online and running")
     print('------')
     print("Log:\n")
     #Set the 'playing' status as the set string
-    await client.change_presence(game=discord.Game(name='!commands | !github',  type = 1, url = 'https://www.twitch.tv/thelittledude_ld'))
+    await botClient.change_presence(game=discord.Game(name='!commands | !github',  type = Streaming, url = twitchLink))
 
 @client.event
 async def on_message(message):
-    if message.content.lower().startswith('!commands'):
+    if messageStarts('!commands'):
         print("!commands command called")
-        await client.send_message(message.channel, "```\nHere are all the commands:\n\n*Not available\n\n!github - Link to the github of Felix Bot\n!alive? - Checking for my pulse I see...\n!gay - Use at your own risk... \n*!image 'search' - Searches Google images```")
-    elif message.content.lower().startswith('!github'):
+        await sendMessage(message.channel, sendCommands)
+    elif messageStarts('!github'):
         print("!github command called")
-        await client.send_message(message.channel, "https://github.com/Jmaonri/Felix")
-    elif message.content.lower().startswith('!alive?'):#The message is transformed into lowercase characters
+        await sendMessage(message.channel, githubLink)
+    elif messageStarts('!alive?'):#The message is transformed into lowercase characters
         print("!alive? command called")
-        await client.send_message(message.channel, "Yes, im alive")
-    elif message.content.lower().startswith('!nut'):
+        await sendMessage(message.channel, "Yes, im alive")
+    elif messageStarts('!nut'):
         print("!nut command called")
-        await client.send_message(message.channel, "https://www.dailydot.com/wp-content/uploads/228/6f/ab64a69d4cd848bad23ed0f9190a91eb.jpg")
-    elif message.content.lower().startswith('!gay'):
+        await sendMessage(message.channel, "https://www.dailydot.com/wp-content/uploads/228/6f/ab64a69d4cd848bad23ed0f9190a91eb.jpg")
+    elif messageStarts('!gay'):
         #AFTER ONE FUCKING HOUR THIS FINALLY WORKS, DONT TOUCH THIS SHIT EVER AGAIN (nvm I broke my rule but I made it better)
         print("!gay command called")
-        embed = discord.Embed(title = "No u faggot")
-        embed.set_image(url = felixPics[0])
-        await client.send_message(message.author, embed = embed)
+        embed.set_image(url = imageList[0])
+        await sendMessage(message.author, discord.Embed(title = "No u faggot"))
 
 #Bot token goes here
-client.run(Token.token) #Opens Token.py and draws the token variable "client.run(file name, variable in file)"
+botClient.run(Token.token) #Opens Token.py and draws the token variable "botClient.run(file name, variable in file)"
